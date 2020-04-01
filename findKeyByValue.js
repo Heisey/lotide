8efed4541156068aab79202af4e4a39fa0208d36
heisey <jheisler01@gmail.com>
2020-04-01 20:42:44 +0000

    finished findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const findKeyByValue = (obj, key) => {
+
+  for (let objKey in obj) {
+    if (obj[objKey] === key) {
+      return objKey
+    }
+  }
+
+  return undefined
+}
+
+// file import
+// const assertEqual = require('./assertEqual')
+
+// tests
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

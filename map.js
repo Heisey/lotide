b1a6096fce6855f4c9bbaebb6d65915210b9b078
heisey <jheisler01@gmail.com>
2020-04-02 15:03:47 +0000

    finished takeUnitl func

--- map.js
+++ map.js
@@ -3,14 +3,14 @@ const map = (array, callback) => {
   const results = [];
 
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
 
-  return results
+  return results;
 
-}
+};
 
-module.exports = map
+module.exports = map;
 
 // tests
 // const words = ["ground", "control", "to", "major", "tom"];
